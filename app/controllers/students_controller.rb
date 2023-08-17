fecf6d8e84bfb3214b170ee0157c21d7093ac381
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-08-16 21:36:54 -0700

    commit tree 94c662279d4927e3140caf04cc0ee1384a2ae790

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,15 @@
 class StudentsController < ApplicationController
 
+  ##this works!!
   def index
-    students = Student.all
+    students = params[:name] ? Student.find_by("first_name = ?", params[:name]) || Student.find_by("last_name = ?", params[:name]) : Student.all
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 end
+
